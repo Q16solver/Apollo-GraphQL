7897c3f862f601f8bb3aa4f474f665d9725c28a2
Jimmy Chen <jimmychen12138@gmail.com>
2021-10-04 13:28:52 +1100

    Added generate random string

--- src/resolvers/user.ts
+++ src/resolvers/user.ts
@@ -1,10 +1,16 @@
+import { makeId } from "@/util/util";
 import "reflect-metadata";
-import { Query, Resolver } from "type-graphql";
+import { Mutation, Query, Resolver } from "type-graphql";
 
 @Resolver()
 export class UserResolver {
   @Query(() => String)
-  async me(): Promise<string> {
-    return "Yes";
+  async randomQuery(): Promise<string> {
+    return makeId(10);
+  }
+
+  @Mutation(() => String)
+  async randomMutation(): Promise<string> {
+    return makeId(10);
   }
 }
