272b39f65775ee0052e8512796490c04dc7d81d5
Jimmy Chen <jimmychen12138@gmail.com>
2021-10-17 19:07:20 +1100

    Added input to creating events and a subscription

--- src/models/event.ts
+++ src/models/event.ts
@@ -1,11 +1,35 @@
 import "reflect-metadata";
-import { Field, ObjectType } from "type-graphql";
+import { Field, InputType, ObjectType } from "type-graphql";
 
 @ObjectType()
 export class Event {
   @Field()
   id: string;
 
+  @Field()
+  category: string;
+
+  @Field()
+  title: string;
+
+  @Field()
+  description: string;
+
+  @Field()
+  location: string;
+
+  @Field()
+  date: string;
+
+  @Field()
+  time: string;
+
+  @Field()
+  organizer: string;
+}
+
+@InputType()
+export class EventInput {
   @Field()
   category: string;
 
--- src/resolvers/event.ts
+++ src/resolvers/event.ts
@@ -1,16 +1,11 @@
-import { Event } from "models/event";
+import { Event, EventInput } from "models/event";
 import "reflect-metadata";
-import { Mutation, Query, Resolver } from "type-graphql";
+import { Arg, Mutation, Publisher, PubSub, Query, Resolver, Root, Subscription } from "type-graphql";
 import { makeId } from "util/util";
 import { v4 as uuidv4 } from "uuid";
 
 @Resolver()
 export class EventResolver {
-  @Query(() => String)
-  async randomQuery(): Promise<string> {
-    return makeId(10);
-  }
-
   @Query(() => Event)
   async event(): Promise<Event> {
     return {
@@ -26,21 +21,17 @@ export class EventResolver {
   }
 
   @Mutation(() => Event)
-  async createEvent(): Promise<Event> {
-    return {
-      id: uuidv4(),
-      category: makeId(10),
-      title: makeId(10),
-      description: makeId(10),
-      location: makeId(10),
-      date: makeId(10),
-      time: makeId(10),
-      organizer: makeId(10),
-    };
+  async createEvent(
+    @Arg("input", () => EventInput) input: EventInput,
+    @PubSub("Event") publish: Publisher<Event>
+  ): Promise<Event> {
+    const event = { id: uuidv4(), ...input };
+    await publish(event);
+    return event;
   }
 
-  @Mutation(() => String)
-  async randomMutation(): Promise<string> {
-    return makeId(10);
+  @Subscription(() => Event, { topics: "Event" })
+  async eventSubscription(@Root() payload: Event): Promise<Event> {
+    return payload;
   }
 }
